54cf7b68ac6ea36f05f5f41465fe83067f74301f
rivergod <rivernile@naver.com>
2012-05-13 20:43:17 +0900

    .

---
+++ src/src/com/krcode/android/editor/vector2d/EditorSurfaceView.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.krcode.android.editor.vector2d;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+/**
+ * @author rivergod
+ *
+ */
+public class EditorSurfaceView extends GLSurfaceView {
+
+	/**
+	 * @param context
+	 */
+	public EditorSurfaceView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public EditorSurfaceView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
