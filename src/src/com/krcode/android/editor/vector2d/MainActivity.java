9e4f0a4e8dd9ace84183ec6462eaf34958a654e8
rivergod <rivernile@naver.com>
2012-06-03 11:38:12 +0900

    . append library min3d -- http://code.google.com/p/min3d/

--- src/src/com/krcode/android/editor/vector2d/MainActivity.java
+++ src/src/com/krcode/android/editor/vector2d/MainActivity.java
@@ -1,36 +1,60 @@
 package com.krcode.android.editor.vector2d;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.List;
+
+import min3d.core.Object3dContainer;
+import min3d.core.RendererActivity;
+import min3d.interfaces.ISceneController;
+import min3d.objectPrimitives.Sphere;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends RendererActivity implements ISceneController {
+	
+	private List<Object3dContainer> objects;
+	
+	public MainActivity() {
+		super();
+
+		this.objects = new ArrayList<Object3dContainer>();
+	}
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		EditorSurfaceView view = new EditorSurfaceView(this);
-		setContentView(view);
+//		EditorSurfaceView view = new EditorSurfaceView(this);
+//		setContentView(view);
+	}
+	
+	@Override
+	public void initScene() {
+		super.initScene();
+		
+		scene.camera().position.setAll(0,0,-10f);
+		
+		// noLights
+
+		// objects
+//		Object3dContainer sphere;
+//		
+//		sphere = new Sphere(3, 10, 10, true, true, true);
+//		sphere.normalsEnabled(false);
+//		scene.addChild(sphere);
+	
+		
+	}
+	
+	@Override
+	public void updateScene() {
+		super.updateScene();
 	}
 
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
-		// TODO Auto-generated method stub
-//		if (menu.size() >= 4) {
-//			if (MainActivity.accountVO != null
-//					&& MainActivity.accountVO.isAvalidable()) {
-//				if (menu.getItem(3).getItemId() == R.id.quick_menu_loginout) {
-//					menu.getItem(3).setTitle("로그아웃");
-//				}
-//			} else {
-//				if (menu.getItem(3).getItemId() == R.id.quick_menu_loginout) {
-//					menu.getItem(3).setTitle("로그인");
-//				}
-//			}
-//		}
 		return super.onPrepareOptionsMenu(menu);
 	}
 
@@ -44,52 +68,13 @@ public class MainActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Intent intent = null;
 		switch (item.getItemId()) {
-//		case R.id.quick_menu_goto:
-//			intent = new Intent(MainActivity.this, GotoLocationActivity.class);
-//
-//			startActivityForResult(intent, MainActivity.GOTOLOCATIONACTIVITY);
-//			return true;
-//		case R.id.quick_menu_recommend:
-//			if (MainActivity.accountVO != null
-//					&& MainActivity.accountVO.isAvalidable() && mapView != null) {
-//				intent = new Intent(MainActivity.this,
-//						RecommendLocationActivity.class);
-//
-//				startActivityForResult(intent,
-//						MainActivity.RECOMMENDLOCATIONACTIVITY);
-//			} else {
-//				Toast.makeText(getApplicationContext(), "로그인 후 사용하실 수 있습니다.",
-//						Toast.LENGTH_SHORT).show();
-//			}
-//			return true;
-//		case R.id.quick_menu_attention:
-//			if (MainActivity.accountVO != null
-//					&& MainActivity.accountVO.isAvalidable()) {
-//				intent = new Intent(MainActivity.this,
-//						AttentionListActivity.class);
-//
-//				GeoPoint point = mapView.getMapCenter();
-//
-//				intent.putExtra("LATITUDE",
-//						String.valueOf(point.getLatitudeE6()));
-//				intent.putExtra("LONGITUDE",
-//						String.valueOf(point.getLongitudeE6()));
-//
-//				startActivityForResult(intent,
-//						MainActivity.ATTENTIONLISTACTIVITY);
-//			} else {
-//				Toast.makeText(getApplicationContext(), "로그인 후 사용하실 수 있습니다.",
-//						Toast.LENGTH_SHORT).show();
-//			}
-//			return true;
-//		case R.id.quick_menu_loginout:
-//			if (MainActivity.accountVO != null
-//					&& MainActivity.accountVO.isAvalidable()) {
-//				MainActivity.accountVO = null;
-//			} else {
-//				startActivity(new Intent(MainActivity.this, LoginActivity.class));
-//			}
-//			return true;
+		case R.id.quick_menu_append:
+			Object3dContainer sphere;
+			
+			sphere = new Sphere(3, 10, 10, true, true, true);
+			sphere.normalsEnabled(false);
+			scene.addChild(sphere);
+			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
